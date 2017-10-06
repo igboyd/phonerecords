5905604c83c0f79063e5dd5ee2e106193f97e68a
Ian Boyd <C-ian.boyd@charter.com>
2017-10-06 15:59:50 -0600

    stuff

---
+++ src/main/java/com/igb/phonedata/DataLoader.java
@@ -0,0 +1,25 @@
+package com.igb.phonedata;
+
+
+
+public class DataLoader {
+
+    public void loadVoice(){
+
+
+    }
+
+    public void loadMessage(){
+
+    }
+
+    private CsvSchema getVoiceSchema(){
+
+    }
+
+    public static void main(String[] args) throws Exception{
+
+    }
+
+
+}
---
+++ src/main/java/com/igb/phonedata/PhonedataApplication.java
@@ -0,0 +1,14 @@
+package com.igb.phonedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonedataApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonedataApplication.class, args);
+	}
+}
